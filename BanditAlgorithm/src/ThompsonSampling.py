a00b7bfbde1f3c2ca1baba2e4f8cca6e4ef5a6f3
yuyafukuchi <yuya3542@yahoo.co.jp>
2020-09-27 02:04:16 +0900

    add thompson_samplong

---
+++ BanditAlgorithm/src/ThompsonSampling.py
@@ -0,0 +1,13 @@
+import numpy as np
+from np.random import beta
+from Arm import Arm
+
+def thompson_sampling(arms, T):
+    reward = 0
+    for i in range(T):
+        # 事後分布から乱数を生成。これをpの推定値として扱う
+        rand_gened_params = [beta(a=arm.success+1, b=arm.fail+1) for arm in arms]
+        # 推定値が一番高いアームを選択
+        max_index = np.argmax(rand_gened_params)
+        reward += arms[max_index].play()
+    return reward
