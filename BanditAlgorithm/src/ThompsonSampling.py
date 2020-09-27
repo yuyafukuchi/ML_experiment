909aae141b7ecae13bfb8525a50b8ce1e33facd3
yuyafukuchi <yuya3542@yahoo.co.jp>
2020-09-27 16:33:31 +0900

    added hist

--- BanditAlgorithm/src/ThompsonSampling.py
+++ BanditAlgorithm/src/ThompsonSampling.py
@@ -4,10 +4,13 @@ from Arm import Arm
 
 def thompson_sampling(arms, T):
     reward = 0
+    hist = []
     for i in range(T):
         # 事後分布から乱数を生成。これをpの推定値として扱う
         rand_gened_params = [beta(a=arm.success+1, b=arm.fail+1) for arm in arms]
         # 推定値が一番高いアームを選択
         max_index = np.argmax(rand_gened_params)
-        reward += arms[max_index].play()
-    return reward
+        tmp = arms[max_index].play()
+        reward += tmp
+        hist.append(tmp)
+    return reward,hist
