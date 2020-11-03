3eb55a83ff9156ec9a582372ad95e46cfa63060b
yuyafukuchi <yuya3542@yahoo.co.jp>
2020-11-03 17:27:22 +0900

    monkeytype apply

--- MonkeyType/main.py
+++ MonkeyType/main.py
@@ -1,9 +1,12 @@
 from module import add,mul
+import monkeytype
 
 def main():
     print(add(1,3))
-    return
 
+    with monkeytype.trace():
+        mul(2,2)
+    return
 
 if __name__ == '__main__':
     main()
--- MonkeyType/module.py
+++ MonkeyType/module.py
@@ -1,5 +1,5 @@
 def add(a: int,b: int) -> int:
     return a+b
 
-def mul(a,b):
+def mul(a: int,b: int) -> int:
     return a*b
