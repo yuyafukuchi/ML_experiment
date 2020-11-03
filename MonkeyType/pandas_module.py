6f534a7e298a45ce274254b8a13b4e2bc27da253
yuyafukuchi <yuya3542@yahoo.co.jp>
2020-11-03 17:46:46 +0900

    type annotation

--- MonkeyType/pandas_module.py
+++ MonkeyType/pandas_module.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
+from typing import Dict, List
 
-def convert_dict_to_df(table):
+def convert_dict_to_df(table: Dict[str, List[int]]) -> DataFrame:
     return pd.DataFrame(table)
