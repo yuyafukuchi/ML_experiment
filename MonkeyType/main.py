941fdabbeb0a2b16665f1952f3e9aa2815662796
yuyafukuchi <yuya3542@yahoo.co.jp>
2020-11-03 17:37:59 +0900

    update main

--- MonkeyType/main.py
+++ MonkeyType/main.py
@@ -1,9 +1,13 @@
-from module import add,mul
+from int_module import add,mul
+from str_module import remove_space
+from list_module import double_list
+
 import monkeytype
 
 def main():
     print(add(1,3))
-
+    print(remove_space('I am Stranger.'))
+    print(double_list([1,2,3]))
     with monkeytype.trace():
         mul(2,2)
     return
